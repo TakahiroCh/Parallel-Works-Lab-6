fc60d14a6195c0407d7a9722f6b48406fda23423
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-12-08 22:40:19 +0300

    Scripted auto-commit on change (2020-12-08 22:40:19) by gitwatch.sh

--- src/main/java/ru.Ivan/StorageActor.java
+++ src/main/java/ru.Ivan/StorageActor.java
@@ -1,2 +1,17 @@
-package ru.Ivan;public class StorageActor {
+package ru.Ivan;
+
+import akka.actor.AbstractActor;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class StorageActor extends AbstractActor {
+    private ArrayList<String> servers;
+    private Random random = new Random();
+
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
