dd8d770014ef252e90ba722a6467a0b8cac14c5f
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-12-08 22:56:25 +0300

    Scripted auto-commit on change (2020-12-08 22:56:25) by gitwatch.sh

--- src/main/java/ru.Ivan/DeleteServer.java
+++ src/main/java/ru.Ivan/DeleteServer.java
@@ -1,2 +1,13 @@
-package ru.Ivan;public class DeleteServer {
+package ru.Ivan;
+
+public class DeleteServer {
+    private String server;
+
+    public DeleteServer(String server) {
+        this.server = server;
+    }
+
+    public String getServer() {
+        return server;
+    }
 }
