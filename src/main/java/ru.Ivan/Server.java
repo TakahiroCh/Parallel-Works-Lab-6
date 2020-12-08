eefb83e1002d5bd6957521842e91e125ab68f3fb
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-12-09 00:12:38 +0300

    Scripted auto-commit on change (2020-12-09 00:12:38) by gitwatch.sh

--- src/main/java/ru.Ivan/Server.java
+++ src/main/java/ru.Ivan/Server.java
@@ -86,12 +86,12 @@ public class Server {
     }
 
     private static Route createRoute(ActorRef storage, final Http http) {
-        return route(pathSingleSlash(() -> {
-            parameter(URL, url -> {
-                parameter(COUNT, count -> {
-                    check(storage, http, new Request(url, count));
-                })
-            })
-        }));
+        return route(pathSingleSlash(() ->
+            parameter(URL, url ->
+                parameter(COUNT, count ->
+                    check(storage, http, new Request(url, count))
+                )
+            )
+        ));
     }
 }
