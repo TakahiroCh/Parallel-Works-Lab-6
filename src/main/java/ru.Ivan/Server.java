b150336ca0ba9936ff37e7e861157b2c303e490d
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-12-08 23:09:11 +0300

    Scripted auto-commit on change (2020-12-08 23:09:11) by gitwatch.sh

--- src/main/java/ru.Ivan/Server.java
+++ src/main/java/ru.Ivan/Server.java
@@ -3,16 +3,19 @@ package ru.Ivan;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
 import akka.stream.ActorMaterializer;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.io.IOException;
+
 public class Server {
     final private static String ZOO_HOST = "127.0.0.1:2181";
     final private static int TIME_OUT = 2500;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create("routes");
         ActorRef storage = system.actorOf(Props.create(StorageActor.class));
         final ActorMaterializer materializer = ActorMaterializer.create(system);
@@ -21,7 +24,9 @@ public class Server {
             public void process(WatchedEvent watchedEvent) {
             }
         };
-        ZooKeeper zoo = new ZooKeeper(ZOO_HOST, TIME_OUT, )
+        ZooKeeper zoo = new ZooKeeper(ZOO_HOST, TIME_OUT, empty);
+        final Http http = Http.get(system);
+        ZooServer server = new ZooServer(zoo, storage);
 
     }
 }
---
+++ src/main/java/ru.Ivan/ZooServer.java
@@ -0,0 +1,9 @@
+package ru.Ivan;
+
+import akka.actor.ActorRef;
+import org.apache.zookeeper.ZooKeeper;
+
+public class ZooServer {
+    public ZooServer(ZooKeeper zoo, ActorRef storage) {
+    }
+}
