c097eee9b88926fbcc96826f7b3049f925720669
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-12-08 22:55:41 +0300

    Scripted auto-commit on change (2020-12-08 22:55:41) by gitwatch.sh

--- src/main/java/ru.Ivan/NextServer.java
+++ src/main/java/ru.Ivan/NextServer.java
@@ -1,4 +1,13 @@
 package ru.Ivan;
 
 public class NextServer {
+    private String url;
+
+    public NextServer(String ulr) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
