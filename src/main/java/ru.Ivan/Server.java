c1e022cb099c828e3d217240b337d9496141288c
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-12-08 23:00:48 +0300

    Scripted auto-commit on change (2020-12-08 23:00:48) by gitwatch.sh

--- src/main/java/ru.Ivan/Server.java
+++ src/main/java/ru.Ivan/Server.java
@@ -3,10 +3,18 @@ package ru.Ivan;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.stream.ActorMaterializer;
+import org.apache.zookeeper.ZooKeeper;
 
 public class Server {
+    final private static String ZOO_HOST = "127.0.0.1:2181";
+    final private static int TIME_OUT = 2500;
+
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("routes");
         ActorRef storage = system.actorOf(Props.create(StorageActor.class));
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        ZooKeeper zoo = new ZooKeeper(ZOO_HOST, TIME_OUT, )
+
     }
 }
